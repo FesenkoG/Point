28273e89b6552c44c0d9e9305f4bdc0307078c33
Fesenko Georgy <fesenko.g@gmail.com>
2018-08-28 18:05:42 +0300

    did something

---
+++ Point/Presentation Layer/TabBar/Chats/Cells/ChatCell.swift
@@ -0,0 +1,17 @@
+//
+//  ChatCell.swift
+//  Point
+//
+//  Created by Георгий Фесенко on 28/08/2018.
+//  Copyright © 2018 Георгий Фесенко. All rights reserved.
+//
+
+import UIKit
+
+class ChatCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var avatarImageView: CircleImage!
+    @IBOutlet weak var lastMessageLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+}
---
+++ Point/Presentation Layer/TabBar/Chats/Cells/IncomingCell.swift
@@ -0,0 +1,13 @@
+//
+//  IncomingCell.swift
+//  Point
+//
+//  Created by Георгий Фесенко on 28/08/2018.
+//  Copyright © 2018 Георгий Фесенко. All rights reserved.
+//
+
+import UIKit
+
+class IncomingCell: UITableViewCell {
+    
+}
